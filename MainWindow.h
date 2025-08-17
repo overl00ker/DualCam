13484e7d08ddc2374a5ebc98539dc2f9b0e866fb
Andrew Tymofeev <149320824+overl00ker@users.noreply.github.com>
2025-08-17 14:04:04 +0300

    Update MainWindow.h

--- MainWindow.h
+++ MainWindow.h
@@ -5,11 +5,12 @@
 #include <QTimer>
 #include <QAction>
 #include <QMenuBar>
-#include <QKeyEvent>
 #include <QVBoxLayout>
 #include <QProcess>
 #include <deque>
+#include <utility>
 #include <memory>
+#include <thread>
 #include <opencv2/opencv.hpp>
 #include "capture_backend.h"
 
@@ -20,45 +21,44 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 public:
     explicit MainWindow(const CaptureParams& cam0,
-                        const CaptureParams& cam1,
-                        QWidget* parent = nullptr);
+        const CaptureParams& cam1,
+        QWidget* parent = nullptr);
     ~MainWindow() override;
 
-private slots:
+protected:
+    void showEvent(QShowEvent* e) override;
+
+private:
+    void buildMenus();
+
+    void openCams();
+    void closeCams();
+
     void setOneCam();
     void setTwoCams();
     void setRes640();
     void setRes1280();
     void setFps30();
     void setFps60();
-
     void toggleDiff();
     void toggleGraph();
-
     void toggleFullscreen();
     void restartApp();
     void checkUpdates();
 
-private:
-    void buildMenus();
-    void openCams();
-    void closeCams();
     void updateFrame();
-
     void updateDisplayGeometry();
 
-    [[nodiscard]] cv::Mat ensureGray(const cv::Mat& src);
-    [[nodiscard]] cv::Mat ensureBGR(const cv::Mat& src);
-    double  computeSharp(const cv::Mat& gray);
-    void    drawOverlay(cv::Mat& img, const std::string& text);
+    cv::Mat ensureGray(const cv::Mat& src);
+    cv::Mat ensureBGR(const cv::Mat& src);
     cv::Mat makeGraphImage(int w, int h);
 
 private:
-    CaptureParams p0_, p1_;
+    CaptureParams p0_;
+    CaptureParams p1_;
 
-    int CAP_W_{ 0 }, CAP_H_{ 0 }, DISP_W_{ 0 }, DISP_H_{ 0 };
-
-    LibcameraCapture cam0_, cam1_;
+    LibcameraCapture cam0_;
+    LibcameraCapture cam1_;
     bool cam0_ok_{ false };
     bool cam1_ok_{ false };
 
@@ -67,13 +67,15 @@ private:
 
     QAction* actDiff_{ nullptr };
     QAction* actGraph_{ nullptr };
-    QAction* actFull_{ nullptr };
     QAction* actFreeze_{ nullptr };
+    QAction* actFull_{ nullptr };
 
-    bool showDiff_{ true };
-    bool showGraph_{ true };
-    bool fullscreen_{ false };
+    bool showDiff_{ false };
+    bool showGraph_{ false };
     bool freezeHistory_{ false };
+
+    int CAP_W_{ 0 }, CAP_H_{ 0 };  
+    int DISP_W_{ 0 }, DISP_H_{ 0 }; 
 
     cv::Mat gaussMask_;
 
@@ -86,7 +88,5 @@ private:
     FrameGrabber* grabber0_{ nullptr };
     FrameGrabber* grabber1_{ nullptr };
 
-protected:
-    void showEvent(QShowEvent* e) override;                        
+    std::thread openThread_;
 };
-
